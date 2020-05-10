e19f43456b497654d9babf2865a0ce09dcfde64f
Parker Chun <parkerchun@gmail.com>
2020-05-09 14:39:13 -1000

    formatting audio

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+var song = new Audio("betterTogether.mp3");
+
 $(".ParkerButton").click( function() {
   console.log("clicked");
   $("#NoeLetter").collapse("hide");
@@ -7,4 +9,20 @@ $(".ParkerButton").click( function() {
 $(".NoeButton").click( function() {
   console.log("clicked");
   $("#ParkerLetter").collapse("hide");
+});
+
+$(".carousel-inner").click( function() {
+  console.log("clicked");
+  $("#carouselSlideshow").toggleClass("carousel-fade");
+});
+
+$(".down-arrow").click( function() {
+  console.log("clicked");
+  if(song.paused){
+    song.play();
+  }
+  else{
+    song.pause();
+  }
+
 });
